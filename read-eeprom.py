b952390d894f4300ea4c68736628d2ea9b5e7403
Jurph <FA!Rb0rngit>
2017-11-12 17:18:27 -0500

    Dealing with timeouts and I/O errors

--- read-eeprom.py
+++ read-eeprom.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python2
 NIKE_VENDOR_ID = 0x11ac
 NIKE_PRODUCT_ID = 0x5455
+
+import time
 import usb.core
 import usb.util
 import usb.control
 
-dev = usb.core.find(idVendor=NIKE_VENDOR_ID,  idProduct=NIKE_PRODUCT_ID)
-if dev.is_kernel_driver_active( 0 ) :
+device = usb.core.find(idVendor=NIKE_VENDOR_ID, idProduct=NIKE_PRODUCT_ID)
+device.reset()
+if device.is_kernel_driver_active(0):
     print("Detaching kernel driver\n")
-    dev.detach_kernel_driver(0)
+    device.detach_kernel_driver(0)
 
-configuration = usb.util.find_descriptor(dev, bConfigurationValue=1)
+configuration = usb.util.find_descriptor(device, bConfigurationValue=1)
 interface = configuration[(0, 0)]
 
 #  This is a packet that reads data out of the EEPROM on board.
@@ -22,15 +25,15 @@ bytes_num = [0x09, 0x05, 0xb3, 0x10, 0x00, 0x00, 0x00, 0x00,
              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-data_raw = "" .join(map(chr,  bytes_num))
+data_raw = "".join(map(chr, bytes_num))
 
 while True:
-    #Clean  outstanding
+    # Clean outstanding
     try:
-        outs = interface [ 0].read(64)
+        outs = interface[0].read(64)
         print("Outstanding: ")
-        print ( outs )
-    except: 
+        print (outs)
+    except:
         break
 
 print("Writing packet ")
@@ -38,6 +41,12 @@ print("Writing packet ")
 interface[1].write(data_raw)
 o = open("raw1.OUT", "wb")
 
+
 while True:
     data = interface[0].read(64)[:]
     print(data[:])
+    o.write("".join(map(chr, data)))
+    nop = interface[0].read(0)[:]
+
+
+    # usb.util.dispose_resources(device)
